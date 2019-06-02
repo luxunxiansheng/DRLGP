9f3e7e082aa4552d6835ac1b87df7c7f4993d65d
Bin.Li <ornot2008@yahoo.com>
2019-06-02 20:24:08 +0800

    wip

--- common/point.py
+++ common/point.py
@@ -4,3 +4,5 @@ from collections import namedtuple
 class Point(namedtuple('Point', 'row col')):
     def __deepcopy__(self, memodict={}):
         return self
+    def __eq__(self,other):
+        return self.row == other.row and self.col == other.col
---
+++ generate_alphazero_games.py
@@ -0,0 +1,98 @@
+import argparse
+
+import numpy as np
+from agent.alphazeroagent import AlphaZeroAgent
+from common.board import Board
+from common.move import Move
+from common.oneplaneencoder import OnePlaneEncoder
+from common.point import Point
+from game.connect5game import Connect5Game
+from tqdm import tqdm
+
+
+def experiment(players, start_player):
+    
+    boards= []
+    moves = []
+                
+    game = Connect5Game(3, players, start_player)
+    encoder = OnePlaneEncoder(game.working_game_state.board)
+    
+    while not game.is_over():
+        move = game.working_game_state.player_in_action.select_move(
+            game, game.working_game_state)
+
+        board_matrix= encoder.encode(game._working_game_state)
+        boards.append(board_matrix)
+
+        move_one_hot = np.zeros(encoder.num_points())
+        move_one_hot[encoder.encode_point(move.point)] =1
+        moves.append(move_one_hot)   
+
+        game.apply_move(move)
+        
+        # game.working_game_state.board.print_board()
+    winner = game.get_winner(game.working_game_state)
+    return winner,np.array(boards), np.array(moves)
+
+
+def load_generated_data():
+    boards = np.load('./generated_data/features.npy')
+    moves  = np.load('./generated_data/labels.npy')
+
+    return boards, moves
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-games','-n',type=int, default=20)
+    
+    args= parser.parse_args()
+
+    total_games = args.num_games
+
+    players = [AlphaBetaAgent(0, "AlphaBetaAgentX","X"),
+               AlphaBetaAgent(1, "AlphaBetaAgentO","O")]
+
+    start_player = players[1]
+
+    win_counts = {
+        players[0].name: 0,
+        players[1].name: 0,
+        "Draw":          0,
+ 
+    }
+
+    features=[]
+    labels=[]
+
+    for _ in tqdm(range(0, total_games)):
+        winner,x,y = experiment(players, start_player)
+        features.append(x)
+        labels.append(y)
+        
+        if winner is not None:
+            win_counts[winner.name] += 1
+        else:
+            win_counts["Draw"] += 1
+
+    print("************************************************************************************")
+    print("{} plays fisrt".format(start_player.name))
+    print("------------------------------------------------------------------------------------")
+    print("{} win ratio is {:.2f}".format(players[0].name, float(
+        win_counts[players[0].name])/float(total_games)))
+    print("------------------------------------------------------------------------------------")
+    print("{} win ratio is {:.2f}".format(players[1].name, float(
+        win_counts[players[1].name])/float(total_games)))
+    print("------------------------------------------------------------------------------------")
+    print("Draw ratio is {:.2f}".format(float(
+        win_counts["Draw"])/float(total_games)))
+    print("************************************************************************************")
+
+    np.save('./generated_data/features.npy',np.concatenate(features))
+    np.save('./generated_data/labels.npy',np.concatenate(labels))  
+
+
+if __name__ == '__main__':
+    main()
