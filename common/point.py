4a87912c7155ed0307a82a9d7279498335305a24
Bin.Li <ornot2008@yahoo.com>
2019-06-04 10:18:50 +0800

    wip

--- common/point.py
+++ common/point.py
@@ -4,5 +4,4 @@ from collections import namedtuple
 class Point(namedtuple('Point', 'row col')):
     def __deepcopy__(self, memodict={}):
         return self
-    def __eq__(self,other):
-        return self.row == other.row and self.col == other.col
+    
