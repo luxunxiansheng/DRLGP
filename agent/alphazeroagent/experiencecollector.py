619d45f1594b6cbc3d837f146700cd0609438099
Bin.Li <ornot2008@yahoo.com>
2019-11-29 09:54:48 +0800

    refactor  experience collector

--- agent/alphazeroagent/experiencecollector.py
+++ agent/alphazeroagent/experiencecollector.py
@@ -39,21 +39,19 @@ class ExperienceCollector:
         self._states = []
         self._visit_counts = []
         self._rewards = []
-        self._current_episode_states = []
-        self._current_episode_visit_counts = []
+        
 
     def reset(self):
-        self._current_episode_states = []
-        self._current_episode_visit_counts = []
+        self._states = []
+        self._visit_counts = []
+        self._rewards = []
 
     def record_decision(self, state, visit_counts):
-        self._current_episode_states.append(state)
-        self._current_episode_visit_counts.append(visit_counts)
+        self._states.append(state)
+        self._visit_counts.append(visit_counts)
 
     def complete_episode(self, reward):
-        num_states = len(self._current_episode_states)
-        self._states += self._current_episode_states
-        self._visit_counts += self._current_episode_visit_counts
+        num_states = len(self._states)
         self._rewards += [reward for _ in range(num_states)]
         self.reset()
 
