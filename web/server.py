45ef7e8f2fe40c0862bf19d2d4b904fc8935d0c1
Bin.Li <bin.li@jfhealthcare.com>
2020-01-04 10:15:47 +0800

    server work

--- web/server.py
+++ web/server.py
@@ -40,18 +40,20 @@ import sys
 import torch
 import torch.nn as nn
 from flask import Flask, jsonify, request
-from models.ResNet8Network import ResNet8Network
 
 from agent.alphazeroagent.alphazeroagent import AlphaZeroAgent
 from agent.alphazeroagent.experiencebuffer import ExpericenceBuffer
 from agent.alphazeroagent.experiencecollector import ExperienceCollector
+from agent.alphazeroagent.mcts.tree import Tree
 from agent.humanplayer import HumanPlayer
 from boardencoder.snapshotencoder import SnapshotEncoder
+from boardencoder.deepmindencoder import DeepMindEncoder 
 from common.board import Board
 from common.move import Move
 from common.player import Player
 from common.point import Point
 from game.connect5game import Connect5Game
+from models.ResNet8Network import ResNet8Network
 
 sys.path.insert(0, os.path.abspath('.'))
 
@@ -78,13 +80,13 @@ def get_web_app():
     def select_move(bot_name):
         historic_jboard_positions = [point_from_coords([move][0]) for move in (request.json)['moves']]
         historic_moves = [Move(Point(board_size+1-historic_jboard_position.row, historic_jboard_position.col)) for historic_jboard_position in historic_jboard_positions]
-        game = Connect5Game(board, players, start_player, False)
+        game = Connect5Game(board, players, start_player,number_of_planes, False)
         for move in historic_moves:
             if isinstance(game.working_game_state.player_in_action, AlphaZeroAgent):
                 game.working_game_state.player_in_action.store_game_state(game.working_game_state)
             game.apply_move(move)
 
-        bot_move = bot_agent.select_move(game, game.working_game_state)
+        bot_move = bot_agent.select_move(game)
         game.apply_move(bot_move)
         game.working_game_state.board.print_board()
         jboard_postion = Point(board_size+1-bot_move.point.row, bot_move.point.col)
@@ -95,7 +97,7 @@ def get_web_app():
         return jsonify({
             'bot_move': bot_move_str,
             'over': over,
-            'diagnostics': bot_agent.diagnostics
+            'diagnostics': None
         })
 
     return app
@@ -103,21 +105,24 @@ def get_web_app():
 
 torch.manual_seed(1)
 use_cuda = torch.cuda.is_available()
-the_device = torch.device('cuda' if use_cuda else 'cpu')
+device = torch.device('cuda' if use_cuda else 'cpu')
 
-number_of_planes = 10
-board_size = 9
-round_per_moves = 100
+number_of_planes = 4
+board_size = 8
+az_mcts_round_per_moves = 500
+c_puct = 8.0
+az_mcts_temperature=0.001
 
-encoder = SnapshotEncoder(number_of_planes, board_size)
+encoder = DeepMindEncoder(number_of_planes, board_size)
 
 input_shape = (number_of_planes, board_size, board_size)
 model_new = ResNet8Network(input_shape, board_size * board_size)
-# model_new.load_state_dict(torch.load('./archived_model/new/1.pth'))
+model_new.load_state_dict(torch.load('./checkpoints/debug_resnet_4_5_deepmind/best.pth.tar'))
 model_new.eval()
 
-bot_agent = AlphaZeroAgent(2, "Agent_New", "2", encoder, model_new, round_per_moves, None, device=the_device)
-human_agent = HumanPlayer(1, "HumanPlayerX", "1")
+mcts_tree = Tree()
+bot_agent = AlphaZeroAgent(Connect5Game.ASSIGNED_PLAYER_ID_2, "Agent_New", encoder, model_new,mcts_tree,az_mcts_round_per_moves, c_puct, az_mcts_temperature,device=device)
+human_agent = HumanPlayer(Connect5Game.ASSIGNED_PLAYER_ID_1, "HumanPlayerX")
 
 players = [bot_agent, human_agent]
 start_player = human_agent
